b0ffbae68b6faad06f164f679746e14a2cbff2ee
RobertHan96 <yshan4329@gmail.com>
2020-05-12 20:59:14 +0900

    Initial commit : 프로젝트 초기 설정, 회원가입 레이아웃

---
+++ app/src/main/java/com/nexon/colosseum/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.nexon.colosseum
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
