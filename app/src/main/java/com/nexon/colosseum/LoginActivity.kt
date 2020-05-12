ab2fbff137701f2184a298609a917751c6a0f3c5
RobertHan96 <yshan4329@gmail.com>
2020-05-12 22:07:13 +0900

    5th commit : 자동 로그인 기능, 로그인액티비티로 이름 변경

--- app/src/main/java/com/nexon/colosseum/LoginActivity.kt
+++ app/src/main/java/com/nexon/colosseum/LoginActivity.kt
@@ -5,18 +5,23 @@ import android.util.Log
 import android.widget.Toast
 import com.nexon.colosseum.utils.ContextUtil
 import com.nexon.colosseum.utils.ServerUtil
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_login.*
 import org.json.JSONObject
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         setupEvents()
         setValues()
     }
 
     override fun setupEvents() {
+        autoLoginCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+            ContextUtil.setAutoLogin(mContext, isChecked)
+
+        }
+
         loginBtn.setOnClickListener {
             val email = emailEdt.text.toString()
             val password = passwordEdt.text.toString()
@@ -24,6 +29,7 @@ class MainActivity : BaseActivity() {
             ServerUtil.postRequestLogin(mContext, email, password, object : ServerUtil.JsonResponseHandler{
                 override fun onResponse(json: JSONObject) {
                     Log.d("서버 응답",  json.toString())
+
                     val code = json.getInt("code")
 
                     if (code == 200) {
@@ -49,7 +55,7 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-
+        autoLoginCheckBox.isChecked = ContextUtil.isAutoLogin(mContext)
     }
 
 
