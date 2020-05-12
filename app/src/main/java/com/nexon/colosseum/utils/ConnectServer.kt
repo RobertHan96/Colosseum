50db8c304a47654d87965ba9b25243bf5d5508b4
RobertHan96 <yshan4329@gmail.com>
2020-05-12 21:16:06 +0900

    2nd commit : 아마존서버 연결, 네트워크 로그인 추가

---
+++ app/src/main/java/com/nexon/colosseum/utils/ConnectServer.kt
@@ -0,0 +1,164 @@
+package com.nexon.colosseum.utils
+
+import android.content.Context
+import android.util.Log
+import okhttp3.*
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import org.json.JSONObject
+import java.io.IOException
+
+class ConnectServer {
+
+    interface JsonResponseHandler {
+        fun onResponse(json: JSONObject)
+    }
+
+    companion object {
+
+        private val BASE_URL = "http://ec2-15-165-177-142.ap-northeast-2.compute.amazonaws.com"
+
+        fun postRequestLogin(
+            context: Context,
+            id: String,
+            pw: String,
+            handler: JsonResponseHandler?
+        ) {
+
+            val client = OkHttpClient()
+            val urlStr = "${BASE_URL}/user"
+
+            val formBody = FormBody.Builder()
+                .add("email ", id)
+                .add("password", pw)
+                .build()
+
+            val request = Request.Builder()
+                .url(urlStr)
+                .post(formBody)
+//                .header()  => API가 헤더를 요구하면 추가해야함.
+                .build()
+
+            client.newCall(request).enqueue(object : Callback {
+
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+                    val body = response.body!!.string()
+                    val json = JSONObject(body)
+
+                    handler?.onResponse(json)
+
+
+                }
+
+            })
+
+
+        }
+//
+//        fun getRequestMyInfo(context: Context, handler: JsonResponseHandler?) {
+//
+//            val client = OkHttpClient()
+//            val urlBuilder = "${BASE_URL}/my_info".toHttpUrlOrNull()!!.newBuilder()
+//            urlBuilder.addEncodedQueryParameter("device_token", "임시기기토큰")
+//            urlBuilder.addEncodedQueryParameter("os", "Android")
+//
+//            val urlStr = urlBuilder.build().toString()
+//
+////            Log.d("완성된주소", urlStr)
+//
+//            val request = Request.Builder()
+//                .url(urlStr)
+//                .header("X-Http-Token", ContextUtil.getUserToken(context))
+//                .build()
+//
+//            client.newCall(request).enqueue(object : Callback {
+//                override fun onFailure(call: Call, e: IOException) {
+//                    e.printStackTrace()
+//                }
+//                override fun onResponse(call: Call, response: Response) {
+//                    val body = response.body!!.string()
+//                    val json = JSONObject(body)
+//                    handler?.onResponse(json)
+//                }
+//            })
+//        }
+//
+//        fun getRequestPostList(context: Context, handler: JsonResponseHandler?) {
+//
+//            val client = OkHttpClient()
+//            val urlBuilder = "${BASE_URL}/black_list".toHttpUrlOrNull()!!.newBuilder()
+////            urlBuilder.addEncodedQueryParameter("device_token", "임시기기토큰")
+////            urlBuilder.addEncodedQueryParameter("os", "Android")
+//
+//            val urlStr = urlBuilder.build().toString()
+//
+////            Log.d("완성된주소", urlStr)
+//
+//            val request = Request.Builder()
+//                .url(urlStr)
+//                .header("X-Http-Token", ContextUtil.getUserToken(context))
+//                .build()
+//
+//            client.newCall(request).enqueue(object : Callback {
+//                override fun onFailure(call: Call, e: IOException) {
+//                    e.printStackTrace()
+//                }
+//                override fun onResponse(call: Call, response: Response) {
+//                    val body = response.body!!.string()
+//                    val json = JSONObject(body)
+//                    handler?.onResponse(json)
+//                }
+//            })
+//        }
+//
+//        fun postRequestBlackList(
+//            context: Context,
+//            title: String,
+//            phoneNum: String,
+//            content: String,
+//            handler: JsonResponseHandler?
+//        ) {
+//
+//            val client = OkHttpClient()
+//            val urlStr = "${BASE_URL}/black_list"
+//
+//            val formBody = FormBody.Builder()
+//                .add("title", title)
+//                .add("phone_num", phoneNum)
+//                .add("content", content)
+//                .build()
+//
+//            val request = Request.Builder()
+//                .url(urlStr)
+//                .post(formBody)
+//                .header("X-Http-Token", ContextUtil.getUserToken(context))
+//                .build()
+//
+//            client.newCall(request).enqueue(object : Callback {
+//
+//                override fun onFailure(call: Call, e: IOException) {
+//                    e.printStackTrace()
+//                }
+//
+//                override fun onResponse(call: Call, response: Response) {
+//
+//                    val body = response.body!!.string()
+//                    val json = JSONObject(body)
+//
+//                    handler?.onResponse(json)
+//
+//
+//                }
+//
+//            })
+//
+//
+//        }
+    }
+
+
+}
