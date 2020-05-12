c337afedde6f2df2d3234c96cb201d9799c26509
RobertHan96 <yshan4329@gmail.com>
2020-05-12 21:49:47 +0900

    4th commit : 로그인 성공시 토스트안내, 토큰 저장

--- app/src/main/java/com/nexon/colosseum/MainActivity.kt
+++ app/src/main/java/com/nexon/colosseum/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nexon.colosseum
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.nexon.colosseum.utils.ContextUtil
 import com.nexon.colosseum.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
@@ -24,7 +25,15 @@ class MainActivity : BaseActivity() {
                 override fun onResponse(json: JSONObject) {
                     Log.d("서버 응답",  json.toString())
                     val code = json.getInt("code")
+
                     if (code == 200) {
+                        val data = json.getJSONObject("data")
+                        val token = data.getString("token")
+                        ContextUtil.setUserToken(mContext, token)
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, resources.getString(R.string.login_success_message), Toast.LENGTH_SHORT).show()
+                        }
 
                     } else {
                         val message = json.getString("message")
