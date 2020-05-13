a1be79e09da83046bfa1aa7d8fa1b99ec8427547
RobertHan96 <yshan4329@gmail.com>
2020-05-13 22:20:09 +0900

    12th commit : 이메일 중복 검사 결과 화면 반영 로직 추가2  - 입력한 내용 변경 감지에 따라 안내문구 변경

--- app/src/main/java/com/nexon/colosseum/SignUpActivity.kt
+++ app/src/main/java/com/nexon/colosseum/SignUpActivity.kt
@@ -3,12 +3,16 @@ package com.nexon.colosseum
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import com.nexon.colosseum.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
+    var isEmailCheckOk = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -17,21 +21,39 @@ class SignUpActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        emailEdt.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                isEmailCheckOk = false
+                isDupeEmailText.setText(R.string.email_dupe_check_text)
+                isDupeEmailText.setTextColor(resources.getColor(R.color.veryLightGray))
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+        })
+
         email_dupe_check_btn.setOnClickListener {
             val email = emailEdt.text.toString()
             val dupeCheckType = "EMAIL"
 
             ServerUtil.getRequestIsEmailDuplecated(mContext, dupeCheckType ,email, object : ServerUtil.JsonResponseHandler{
-                @SuppressLint("ResourceAsColor")
                 override fun onResponse(json: JSONObject) {
                     Log.d("중복 확인 결과", json.toString())
                     val code = json.getInt("code")
                     if (code == 200) {
+                        isEmailCheckOk = true
                         runOnUiThread {
                             isDupeEmailText.setTextColor(resources.getColor(R.color.azule))
                             isDupeEmailText.setText(R.string.email_is_not_dupe_text)
                         }
                     } else {
+                        isEmailCheckOk = false
                         runOnUiThread {
                             isDupeEmailText.setTextColor(resources.getColor(R.color.grapefruit))
                             isDupeEmailText.setText(R.string.email_is_dupe_text)
