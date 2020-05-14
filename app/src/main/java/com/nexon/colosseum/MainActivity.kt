2b1f8eadbfb7bb69f99d4461e66cfdfbd0c6d69b
RobertHan96 <yshan4329@gmail.com>
2020-05-14 20:08:52 +0900

    15th commit : 선택한 이미지를 프로필이미지로 바꾸는 기능 추가 - 이미지픽 인텐트, Glide 활용

--- app/src/main/java/com/nexon/colosseum/MainActivity.kt
+++ app/src/main/java/com/nexon/colosseum/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.nexon.colosseum
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+    val REQ = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,7 +19,12 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-        myProfileImg.
+        changeProfileImgBtn.setOnClickListener {
+            val myIntent = Intent(Intent.ACTION_PICK)
+            myIntent.setType("image/*")
+            myIntent.setType(MediaStore.Images.Media.CONTENT_TYPE)
+            startActivityForResult(myIntent, REQ)
+        }
 
     }
 
@@ -21,4 +32,14 @@ class MainActivity : BaseActivity() {
 
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == REQ) {
+            if (resultCode == Activity.RESULT_OK) {
+                Log.d("이미지 선택 결과", data?.data.toString())
+                Glide.with(mContext).load(data?.data).into(myProfileImg)
+            }
+        }
+    }
 }
