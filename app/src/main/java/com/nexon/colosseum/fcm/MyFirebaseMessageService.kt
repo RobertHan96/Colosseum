aeee67285b1cbdadc73a58ca46828b22d4a0ffd5
RobertHan96 <yshan4329@gmail.com>
2020-05-18 21:46:02 +0900

    23th commit : 앱실행 중 푸시메세지 수신시 수행할 로직 추가

--- app/src/main/java/com/nexon/colosseum/fcm/MyFirebaseMessageService.kt
+++ app/src/main/java/com/nexon/colosseum/fcm/MyFirebaseMessageService.kt
@@ -1,8 +1,11 @@
 package com.nexon.colosseum.fcm
 
+import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import java.util.logging.Handler
 
 class MyFirebaseMessageService : FirebaseMessagingService() {
 
@@ -12,10 +15,17 @@ class MyFirebaseMessageService : FirebaseMessagingService() {
         Log.d("신규발급 토큰", token)
     }
 
-//  푸시알림이 수신되면 실행되는 함수
+
+    //  푸시알림이 수신되면 실행되는 함수
     override fun onMessageReceived(message: RemoteMessage?) {
         super.onMessageReceived(message)
         Log.d("메세지 수신", "완료")
-    }
+        Log.d("푸시 제목", message?.notification?.title)
+        Log.d("푸시 내용", message?.notification?.body.toString())
+        android.os.Handler(Looper.getMainLooper()).post {
+            Toast.makeText(applicationContext, message?.notification?.body.toString(), Toast.LENGTH_SHORT).show()
+
+        }
+}
 
 }
